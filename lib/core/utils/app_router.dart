05bea01d8af1be63ce087d969e7f94dc5946bb95
Omar <omarbro224@gmail.com>
2025-10-09 21:28:41 +0300

    create custom search field

--- lib/core/utils/app_router.dart
+++ lib/core/utils/app_router.dart
@@ -1,11 +1,13 @@
 import 'package:bookly_app/features/home/presentation/views/book_details_view.dart';
 import 'package:bookly_app/features/home/presentation/views/home_view.dart';
+import 'package:bookly_app/features/search/presentation/views/search_view.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:go_router/go_router.dart';
 
 abstract class AppRouter {
   static const kHomeView = '/homeView';
   static const kBookDetailsView = '/BookDetailsView';
+  static const kSearchView = '/SearchView';
   static final router = GoRouter(
     routes: [
       GoRoute(
@@ -24,6 +26,11 @@ abstract class AppRouter {
         path: kBookDetailsView,
         builder: (context, state) {
           return const BookDetailsView();
+        },
+      ),GoRoute(
+        path: kSearchView,
+        builder: (context, state) {
+          return const SearchView();
         },
       ),
     ],
