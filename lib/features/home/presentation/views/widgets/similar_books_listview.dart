e22b56ae6df7f50fa5ebb1ed554f12aa344e451b
Omar <omarbro224@gmail.com>
2025-10-13 17:26:59 +0300

    fetch similar books

--- lib/features/home/presentation/views/widgets/similar_books_listview.dart
+++ lib/features/home/presentation/views/widgets/similar_books_listview.dart
@@ -1,25 +1,44 @@
+import 'package:bookly_app/features/home/presentation/views/manager/similar_books_cubit/similar_books_cubit.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class SimilarBooksListView extends StatelessWidget {
   const SimilarBooksListView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height * 0.14,
-      child: ListView.builder(
-        scrollDirection: Axis.horizontal,
-        itemCount: 10,
-        itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 4),
-            child: CustomBookImage(
-              imageUrl: 'https://covers.openlibrary.org/b/id/5546156-L.jpg',
+    return BlocBuilder<SimilarBooksCubit, SimilarBooksState>(
+      builder: (context, state) {
+        if (state is SimilarBooksSuccess) {
+          return SizedBox(
+            height: MediaQuery.of(context).size.height * 0.14,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 4),
+                  child: CustomBookImage(
+                    imageUrl:
+                        state.books,
+                  ),
+                );
+              },
             ),
           );
-        },
-      ),
+        } else if (state is SimilarBooksFailure) {
+          return Center(
+            child: Text(state.errMessage),
+          );
+        } else {
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          );
+        }
+      },
     );
   }
 }
